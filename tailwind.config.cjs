adbd3c7581e713749a191fc0a7e41b0b94ca85d3
shatabdi12 <shatabdi.dey12@gmail.com>
2025-04-21 23:56:16 +0200

    chore: update dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Ensure these paths are correct
+  ],
   theme: {
     extend: {},
   },
